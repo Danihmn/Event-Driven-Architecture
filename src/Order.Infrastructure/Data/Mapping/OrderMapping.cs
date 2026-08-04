c21bff099630e41b9aea9dae486fba71e0a25913
Danihmn <daniel.bezerra.mult@gmail.com>
2026-08-03 23:31:59 -0300

    Codificando mapeamento dos bancos de dados

---
+++ src/Order.Infrastructure/Data/Mapping/OrderMapping.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Order.Infrastructure.Data.Mapping;
+
+public class OrderMapping : IEntityTypeConfiguration<Domain.Entities.Order>
+{
+    public void Configure(EntityTypeBuilder<Domain.Entities.Order> builder)
+    {
+        builder.ToTable("orders");
+
+        builder.HasKey(o => o.Id).HasName("pk_order");
+
+        builder.Property(o => o.Quantity).IsRequired();
+        builder.Property(o => o.Status).HasConversion<string>();
+        builder.Property(o => o.CreatedAt).HasDefaultValue(DateTime.UtcNow);
+    }
+}
---
+++ src/Order.Infrastructure/Data/OrderDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Order.Infrastructure.Data;
+
+public class OrderDbContext : DbContext
+{
+    public DbSet<Domain.Entities.Order> Orders { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        => modelBuilder.ApplyConfigurationsFromAssembly(typeof(OrderDbContext).Assembly);
+}
